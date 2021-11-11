6cf99726caaa9c5cb3184d0c1b0988d9058161dd
Bill44-NotAlone <denyc9@mail.ru>
2021-11-11 11:28:13 +0300

    Версия 0.6

--- HomeWork3/HomeWork3/Program.cs
+++ HomeWork3/HomeWork3/Program.cs
@@ -6,20 +6,23 @@ namespace HomeWork3
     {
         static void Main(string[] args)
         {
+            Random random = new Random();
             CollectionClass<int> collection1 = new CollectionClass<int>();
-            for (int i = 0; i < 16; i++) collection1.Add(i + 1);
+            //for (int i = 0; i < 16; i++) collection1.Add(i + 1);
+            Console.WriteLine(collection1.Length);
+            //Console.WriteLine(collection1.GetElement(3));
+            //Console.WriteLine(collection1.GetIndex(12));
+            collection1.RemoveAt(random.Next(32));
+            //for (int i = 0; i < 15; i++) Console.Write(collection1.GetElement(i));
             Console.WriteLine();
-            Console.WriteLine(collection1.GetElement(3));
-            Console.WriteLine(collection1.GetIndex(12));
-            collection1.RemoveAt(15);
-            for (int i = 0; i < 15; i++) Console.Write(collection1.GetElement(i));
+            Console.WriteLine(collection1.Length);
 
             CollectionClass<string> collection2 = new CollectionClass<string>();
             for (int i = 0; i < 16; i++) collection2.Add((i + 1).ToString());
             Console.WriteLine();
-            Console.WriteLine(collection2.GetElement(3));
-            Console.WriteLine(collection2.GetIndex("15"));
-            collection2.RemoveAt(0);
+            //Console.WriteLine(collection2.GetElement(3));
+            //Console.WriteLine(collection2.GetIndex("15"));
+            collection2.RemoveAt(random.Next(32));
             for (int i = 0; i < 15; i++) Console.Write(collection2.GetElement(i));
         }
     }
@@ -32,12 +35,14 @@ namespace HomeWork3
             public Collection NextCollection;
         }
         Collection initialvalue = null;
-        
+        private int length = 0;
+
         public void Add(Type element)
         {
+            length = length + 1;
             Collection collection = new Collection();
             collection.content = element;
-            if(initialvalue == null)
+            if (initialvalue == null)
             {
                 initialvalue = collection;
                 return;
@@ -54,9 +59,9 @@ namespace HomeWork3
         {
             Collection initialvalue1 = initialvalue;
             int i = 0;
-            while(element.ToString() != initialvalue1.content.ToString())
+            while (element.ToString() != initialvalue1.content.ToString())
             {
-                if(initialvalue1.NextCollection == null)
+                if (initialvalue1.NextCollection == null)
                 {
                     return (-1);
                 }
@@ -68,23 +73,36 @@ namespace HomeWork3
         public Type GetElement(int index)
         {
             Collection initialvalue1 = initialvalue;
-            for (int i = 0; i < index; i++)
+            if (length >= index & 0 <= index)
             {
-                initialvalue1 = initialvalue1.NextCollection;
+                for (int i = 0; i < index; i++)
+                {
+                    initialvalue1 = initialvalue1.NextCollection;
+                }
+                return (initialvalue1.content);
             }
-            return (initialvalue1.content);
+            else throw new Exception("Значение индекса не соответствует длине массива! :(");
         }
         public void RemoveAt(int index)
         {
-            if (index == 0) initialvalue = initialvalue.NextCollection;
-            else
+            if (length >= index & 0 <= index & initialvalue != null)
             {
-                Collection initialvalue1 = initialvalue;
-                for (int i = 0; i < index - 1; i++) initialvalue1 = initialvalue1.NextCollection;
-                Collection initialvalue2 = initialvalue1.NextCollection;
-                initialvalue2 = initialvalue2.NextCollection;
-                initialvalue1.NextCollection = initialvalue2;
+                length = length - 1;
+                if (index == 0) initialvalue = initialvalue.NextCollection;
+                else
+                {
+                    Collection initialvalue1 = initialvalue;
+                    for (int i = 0; i < index - 1; i++) initialvalue1 = initialvalue1.NextCollection;
+                    Collection initialvalue2 = initialvalue1.NextCollection;
+                    initialvalue2 = initialvalue2.NextCollection;
+                    initialvalue1.NextCollection = initialvalue2;
+                }
             }
+            //else throw new Exception("Значение индекса не соответствует длине массива! :(");
+        }
+        public int Length
+        {
+            get { return length; }
         }
     }
 }
